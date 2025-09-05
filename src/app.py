b091f88acefccc016f85ccb0a6a5f3b4f7a8bf58
clsumiso <127267773+clsumiso@users.noreply.github.com>
2025-09-05 21:57:12 +0800

    Update greeting message (feature)

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 def greet(name: str) -> str:
-    return f"Hello, {name}! Welcome to Agile DevOps."
+    return f"Hello, {name}! Welcome to CI/CD with Agile DevOps."
 
 class MyClass:
     def __init__(self,name):
